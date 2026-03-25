38641e31bc8168a9ff1f255ff2a6a1414f62c0ef
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-03-25 10:16:26 -0300

    site em manutençao, criacao de aula 1, pasta central

---
+++ repo-js/repo-js-central/script.js
@@ -0,0 +1,7 @@
+// criação variaveis
+const sistema ="Bonifacio-2DS";
+let tecnico="Seu Nome Aqui";
+
+//mostra nno console e tela
+console.log(`Sistema Iniciando: ${sistema}`);
+alert(`Olá, ${tecnico}! Seja Bem-vindo ao sistema`)
