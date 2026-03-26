af870dfcd3e64bf6ce3fe15201e03d9a39df7491
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-03-25 22:33:01 -0300

    att

---
+++ repo-js/aula-01/atividade-02/atividade02.js
@@ -0,0 +1,5 @@
+    const valorPeca = Number(prompt("Qual o valor da peça?"))
+    const valorMaoDeObra = Number(prompt("Qual o valor da Mão de Obra?"))
+    const soma = valorPeca + valorMaoDeObra
+
+    alert(`O valor total ficou ${soma}`)
