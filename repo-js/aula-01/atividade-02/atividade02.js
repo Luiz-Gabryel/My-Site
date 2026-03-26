292ad4cb45761e23d6ea94827b4fbba529b1cb9b
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-03-26 08:56:20 -0300

    att

--- repo-js/aula-01/atividade-02/atividade02.js
+++ repo-js/aula-01/atividade-02/atividade02.js
@@ -3,3 +3,5 @@
     const soma = valorPeca + valorMaoDeObra
 
     alert(`O valor total ficou ${soma}`)
+    document.getElementById("resultado").innerText = `O valor total ficou R$ ${soma.toFixed(2)}`;
+
