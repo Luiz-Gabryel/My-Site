0d0593e1f9c533e9cd34c6f2dd1271b0e079f0e2
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-04-08 21:27:20 -0300

    agora vai

--- controle-estoque/login/script.js
+++ controle-estoque/login/script.js
@@ -9,33 +9,38 @@ function fazerLogin() {
         return;
     }
 
-    if (usuario[0] !== usuario[0].toUpperCase()) {
+    if (usuario[0] == usuario[0].toLowerCase()) {
         alert('O usuário deve começar com letra maiúscula.');
         return;
     }
 
+    var senhaValida = true;
+
     if (senha.length < 12) {
-        tentativas++;
+        senhaValida = false;
         alert('A senha deve ter no mínimo 12 caracteres.');
     } else if (senha === senha.toLowerCase()) {
-        tentativas++;
+        senhaValida = false;
         alert('A senha deve ter pelo menos uma letra maiúscula.');
     } else if (senha === senha.toUpperCase()) {
-        tentativas++;
+        senhaValida = false;
         alert('A senha deve ter pelo menos uma letra minúscula.');
     } else if (!senha.includes('luiz') && !senha.includes('Luiz') && !senha.includes('aster') && !senha.includes('Aster')) {
-        tentativas++;
+        senhaValida = false;
         alert('Senha incorreta.');
     } else if (!senha.includes('picos') && !senha.includes('Picos')) {
-        tentativas++;
+        senhaValida = false;
         alert('Senha incorreta.');
-    } else {
-        localStorage.setItem('usuario', usuario);
-        location.href = '../pagina-inicial/pagina-inicial.html';
+    }
+
+    if (!senhaValida) {
+        tentativas++;
+        if (tentativas >= 3) {
+            location.href = 'https://youtu.be/VH8AHeB-QBs?list=RDVH8AHeB-QBs';
+        }
         return;
     }
 
-    if (tentativas >= 3) {
-        location.href = 'https://youtu.be/VH8AHeB-QBs?list=RDVH8AHeB-QBs';
-    }
+    localStorage.setItem('usuario', usuario);
+    location.href = '../pagina-inicial/pagina-inicial.html';
 }
