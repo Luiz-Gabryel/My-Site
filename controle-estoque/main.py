0f0e8440df3e15caf5b7f7c52564eea64b8cd9a3
Luiz-Gabryel <luizgabryel515@gmail.com>
2026-05-09 10:18:37 -0300

    att

--- controle-estoque/main.py
+++ controle-estoque/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from supabase import create_client
+from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-supabase = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_KEY"))
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client["controle_estoque"]
 
 app = FastAPI()
 
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
+
 from auth_routes import auth_router
+from order_routes import order_router
+
 app.include_router(auth_router)
+app.include_router(order_router)
--- controle-estoque/order_routes.py
+++ controle-estoque/order_routes.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter
 
 order_router = APIRouter(prefix="/order", tags=["order"])
 
-@order_router.get("/lista")
+@order_router.get("/estoque")
 async def estoque():
     """
     Essa é a rota padrão de auntentificação do nosso sistema
     """
-    return {"mensagem": "Voce Acessou a rota de lista"}
+    return {"mensagem": "Voce Acessou a rota de estoque"}
